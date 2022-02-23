a94857756faf96d5669f99939d3af5615409c0ce
Iuliia El Sammak <juliag875@gmail.com>
2022-02-22 20:10:44 -0500

    commit tree 3f04f35504bb3e81f8682ae0fe60d3247925caeb

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,24 @@
 # Write your code here.
+require 'pry'
+
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(speackers)
+  speackers.map do |speacker|
+    "Hello, my name is #{speacker}."
+  end
+end
+
+def assign_rooms(speackers)
+  speackers.map.with_index(1) do |speacker, index|
+      "Hello, #{speacker}! You'll be assigned to room #{index}!"
+  end
+end
+
+def printer(speackers)
+  batch_badge_creator(speackers).map { |badge| puts badge }
+
+  assign_rooms(speackers).map { |room| puts room }
+end
